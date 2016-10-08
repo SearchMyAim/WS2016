d1162462e74a692aaf08ab664d6483624d8643c1
Baar Alexander <baar.alexander@gmail.com>
2016-10-08 10:47:20 +0200

    - Add initial version of Beer Database connection - Database: //localhost:5433

---
+++ DBANW_ue01_baar_lanner/test/at/fhj/swd/BeerTest.java
@@ -0,0 +1,51 @@
+package at.fhj.swd;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.AfterClass;
+import org.junit.Assert;
+
+
+@org.junit.FixMethodOrder(org.junit.runners.MethodSorters.NAME_ASCENDING)
+public class BeerTest extends Assert {
+	static EntityManagerFactory factory;
+	static EntityManager manager;
+	static EntityTransaction transaction;
+	
+	static final String persistenceUnitName = "Beer";
+	
+	
+	@BeforeClass 
+	public static void setup() {
+		factory = Persistence.createEntityManagerFactory(persistenceUnitName);
+		assertNotNull(factory);
+	
+		manager = factory.createEntityManager();
+		assertNotNull(manager);
+		
+		transaction = manager.getTransaction();
+		assertNotNull(transaction);
+	}
+	
+	@Test
+	public void connectionTest() {
+		transaction.begin();
+		
+		
+	}
+	
+	@AfterClass
+	public static void tearDown() {
+		if(manager == null) {
+			return;
+		}
+		manager.close();
+		factory.close();
+	}
+	
+}
