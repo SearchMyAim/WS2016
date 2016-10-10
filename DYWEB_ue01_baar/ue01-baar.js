1bdfb970c517f7ba914df9f366502987bd3d4017
Alexander Baar <baar.alexander@gmail.com>
2016-10-10 22:08:05 +0200

    - Finish Dynamic Web UE01.

--- DYWEB_ue01_baar/ue01-baar.js
+++ DYWEB_ue01_baar/ue01-baar.js
@@ -3,47 +3,50 @@ const url = require("url");
 
 const server = http.createServer(function(request, response) {
     const parsedUrl = url.parse(request.url);
-    var body = "Falsche UrL";
-    var statusCode = 200;
-    var contentType = 'plain';
-    var content = "notnull";
 
-    var year = new Date().getFullYear();
+    var statusCode = 200;
+    var contentType = 'text/html';
+    var content = "notnull";
+    var txth1 = "notnull";
 
     switch(parsedUrl.pathname) {
       case "/static-page":
+        txth1 = "Static Page";
         content = "Baar Alexander";
-        contentType = 'text/html';
         break;
 
-        case "/hello-world":
-          body = "Hello World!";
-          contentType = 'text/plain'
-          break;
+      case "/hello-world":
+        txth1 = "";
+        content = "Hello World!";
+        break;
 
-        case "/date-time":
-          content = new Date().getFullYear() + '-' + new Date().getMonth();
-          contentType = 'text/html';
-          break;
+      case "/date-time":
+        txth1 = "";
+        var date = new Date().toISOString();
+        content = date.substring(0,10) + ' ' + date.substring(11,16);
+        break;
 
-        default:
-          //default types are defined at var definition
-          statusCode = 404;
-          break;
+      default:
+        //404 Error, wrong URL hit.
+        txth1 = "404 Error, Page not found!";
+        content = "I've got a bad feeling about this. \n - Han Solo";
+        statusCode = 404;
+        break;
     }
 
+    //Create HTML
     body = `<!DOCTYPE html>
     <html>
       <head>
-        <meta charset = "utf-8">;
+        <meta charset = "utf-8">
       </head>
       <body>
-        <h1>Static Page</h1>
+        <h1>${txth1}</h1>
         <p>${content}</p>
       </body>
     </html>`;
-    contentType =
 
+    //Response to GET request with the created string.
     response.writeHead(statusCode, {
         'Content-Lengt': Buffer.byteLength(body),
         'Content-Type': contentType
