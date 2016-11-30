8426ff44fc51870c6742089637ad48f4f8af2a67
Alexander Baar <baar.alexander@gmail.com>
2016-11-30 16:25:54 +0100

    DYWEB_ue03_baar - Add .html templates to make the json files more readable. - Add Sign-Up page. - Add writing of the user profil data. - Add parsing existing data to directly access them via a absolut html link.

--- DYWEB_ue03_baar/htmlTemplates.js
+++ DYWEB_ue03_baar/htmlTemplates.js
@@ -1,81 +1,76 @@
 /**
  * Created by Baar Alexander on 28.11.2016.
  */
+const fs = require('fs');
 
 (function() {
 
     /* Main HTML Template. */
     module.exports.getTemplate = function() {
-        return `<!DOCTYPE html>
-                <html lang="en">
-                    <head>
-                        <meta charset="UTF-8">
-                        <title>Titel - {{title}}</title>
-                        <style>
-                            html {
-                                font-family: sans-serif;
-                            }
-                            ul {
-                                list-style-type: none;
-                                margin: 0;
-                                padding: 0;
-                                overflow: hidden;
-                                background-color: #333;
-                            }
-                            
-                            li {
-                                float: left;
-                            }
-                            
-                            li a {
-                                display: block;
-                                color: white;
-                                text-align: center;
-                                padding: 14px 16px;
-                                text-decoration: none;
-                            }
-                            
-                            li a:hover {
-                                background-color: #111;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        {{> (lookup . 'bodyPartial') }}
-                    </body>
-                </html>`;
+        return String(fs.readFileSync('./templates/startpage.html'));
     }
 
     /* Front/Start Page. */
     module.exports.getStartPage = function() {
         return `<ul>
-                  <li><a href="home">Home</a></li>
+                  <li><a href="/">Home</a></li>
                   <li style="float:right"><a class="active" href="login">Login</a></li>
-                  <li style="float:right"><a class="active" href="singup">Sign-Up</a></li>
+                  <li style="float:right"><a class="active" href="signup">Sign-Up</a></li>
                 </ul>`;
     }
 
     /* Sing-Up Page. */
     module.exports.getSignUp = function() {
-        return `<p>Sign-Up Page</p>`;
+        return String(fs.readFileSync('./templates/signup.html'));
+    }
+
+    /* User creation successful. */
+    module.exports.getUserSuccess = function() {
+        return `<ul>
+                  <li><a href="/">Home</a></li>
+                </ul>
+                <h1>it's done</h1>
+                <form action="{{action}}" method="post">
+                    <button type="submit">Profil</button>
+                </form>`;
+    }
+
+    /* User Profil Page. */
+    module.exports.getUserProfil = function() {
+        return String(fs.readFileSync('./templates/userProfil.html'));
     }
 
     /* Error 500. */
     module.exports.getError500 = function() {
-        return `<h2>Error 500<h2>
-                <p>PlaceHolder</p>`;
+        return `<div class="error">
+                    <ul>
+                      <li><a href="/">Home</a></li>
+                    </ul>
+                    <h2>Error 500<h2>
+                    <p>PlaceHolder</p>
+                </div>`;
     }
 
     /* Error 400. */
     module.exports.getError400 = function() {
-        return `<h2>Error 400<h2>
-                <p>Placeholder</p>`;
+        return `<div class="error">
+                    <ul>
+                      <li><a href="/">Home</a></li>
+                    </ul>
+                    <h2>Error 400<h2>
+                    <p>PlaceHolder</p>
+                </div>`;
     }
 
     /* Error 405. */
     module.exports.getError405 = function() {
-        return `<h2>Error 405<h2>
-                <p>Placeholder</p>`;
+        return `<div class="error">
+                    <ul>
+                      <li><a href="/">Home</a></li>
+                    </ul>
+                    <h2>Error 405<h2>
+                    <p>PlaceHolder</p>
+                </div>`;
     }
 
 }());
--- DYWEB_ue03_baar/ue03_server_baar.js
+++ DYWEB_ue03_baar/ue03_server_baar.js
@@ -7,7 +7,14 @@ const   http        = require('http'),
         url         = require('url'),
         hbs         = require('handlebars'),
         fs          = require('fs'),
-        htmlHandler = require("./htmlTemplates");
+        htmlHandler = require("./htmlTemplates"),
+        formidable  = require('formidable');
+
+/* Sub-Directories must exist. */
+const usrDir = "./usrDir/";
+if(!fs.existsSync(usrDir)) {
+    fs.mkdirSync(usrDir);
+}
 
 /* Routes */
 const routes = [
@@ -18,9 +25,22 @@ const routes = [
         }
     },
     {
-        rex: /^\/new\/{0,1}$/,
+        rex: /^\/signup\/{0,1}$/,
         methods: {
             'get': getSignUp
+        }
+    },
+    {
+        rex: /^\/newuser\/{0,1}$/,
+        methods: {
+            'post': postNewUser
+        }
+    },
+    {
+        rex: /^\/[a-zA-Z0-9_]{1,255}$/,
+        methods: {
+            'post': postUserProfil,
+            'get': postUserProfil
         }
     }
 
@@ -29,6 +49,8 @@ const routes = [
 /* Partials */
 hbs.registerPartial('startPage',    htmlHandler.getStartPage());
 hbs.registerPartial('signUp',       htmlHandler.getSignUp());
+hbs.registerPartial('newUser',      htmlHandler.getUserSuccess());
+hbs.registerPartial('userProfil',   htmlHandler.getUserProfil());
 hbs.registerPartial('err500',       htmlHandler.getError500());
 hbs.registerPartial('err400',       htmlHandler.getError400());
 hbs.registerPartial('err405',       htmlHandler.getError405());
@@ -37,26 +59,83 @@ hbs.registerPartial('err405',       htmlHandler.getError405());
 function genError(req, res, error) {
     res.setHeader('Content-Type', 'text/html');
     res.statusCode = error;
-    var param = {
-            bodyPartial: "err" + error.toString(),
-            title: "test"
-        };
-    res.write(layout(param));
+    res.write(layout({
+        bodyPartial: "err" + error.toString(),
+        title: "test"
+    }));
     res.end();
 }
 
 function getStartPage(req, res) {
     res.setHeader('Content-Type', 'text/html');
     res.statusCode = 200;
-    res.write(layout({title: "Start Page", bodyPartial: "startPage"}));
+    res.write(layout({
+        title: "Start Page",
+        bodyPartial: "startPage"
+    }));
     res.end();
 }
 
 function getSignUp(req, res) {
     res.setHeader('Content-Type', 'text/html');
     res.statusCode = 200;
-    res.write(layout({title: "Sign-Up", bodyPartial: "signUp"}));
+    res.write(layout({
+        title: "Sign-Up",
+        bodyPartial: "signUp",
+        action: "newuser"}));
     res.end();
+}
+
+/**
+ *
+ * @param req
+ * @param res
+ */
+function postNewUser(req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+        if(err) throw err;
+        var name = fields.Username;
+        fs.writeFile(usrDir + name + '.json', JSON.stringify(fields), 'utf-8', (err) => {
+            if(err) throw err;
+
+            res.setHeader('Content-Type', 'text/html');
+            res.setHeader('Location', "/user");
+            res.statusCode = 201;
+            res.write(layout({
+                title: "User created",
+                bodyPartial: "newUser",
+                action: name
+            }));
+            res.end();
+        });
+    });
+}
+
+/**
+ *
+ * @param req
+ * @param res
+ * @returns {*}
+ */
+function postUserProfil(req, res) {
+    const name = usrDir + url.parse(req.url).pathname.split("/")[1] + '.json';
+    if(!fs.existsSync(name)) {
+        return genError(req, res, 400);
+    }
+    fs.readFile(name, 'utf-8', (err, data) => {
+        if(err) throw err;
+        const fields = JSON.parse(data);
+
+        res.setHeader('Content-Type', 'text/html');
+        res.statusCode = 200;
+        res.write(layout({
+            title: "User Profil",
+            bodyPartial: 'userProfil',
+            data: fields
+        }));
+        res.end();
+    });
 }
 
 /* Compile HTML Page */
@@ -81,6 +160,4 @@ const server = http.createServer(function(req, res) {
     } catch (err) {
         genError(req, res, 500);
     }
-});
-
-server.listen(3000);
+}).listen(3000);
